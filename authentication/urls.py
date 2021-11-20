51c2e97c5d9255c87566f555e8f88e67482ae47d
jcjfgnl9205 <jcjfgnl9205@gmail.com>
2021-11-20 20:06:32 +0900

    Refresh token

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import RegisterView, VerifyEmail, LoginAPIView, PasswordTokenCheckAPI, RequestPasswordResetEmail, SetNewPasswordAPIView
-
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
@@ -9,4 +11,5 @@ urlpatterns = [
     path('request-reset-email', RequestPasswordResetEmail.as_view(), name='equest-reset-email'),
     path('password-reset/<uidb64>/<token>', PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
     path('password-reset-complete/', SetNewPasswordAPIView.as_view(), name='password-reset-complete'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     ]
